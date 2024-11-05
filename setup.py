bb29be83296dac46b304ac15e4e531c6768e0547
Mateen Khan <mateenmk2428@gmail.com>
2024-11-05 21:35:56 +0530

    Dependencies Installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Approval",
+    version="1.0.0",
+    author="Mateen Khan",
+    author_email="mateenmk2428@gmail.com",
+    packages=find_packages()
+)
