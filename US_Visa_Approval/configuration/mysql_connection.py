cec0dc84958c5deca8378a9d428c193a6ff36fd0
Mateen Khan <mateenmk2428@gmail.com>
2024-11-18 22:27:25 +0530

    Connected MySQL Database

---
+++ US_Visa_Approval/configuration/mysql_connection.py
@@ -0,0 +1,38 @@
+import sys
+from US_Visa_Approval.exception import USvisaException
+from US_Visa_Approval.logger import logging
+from US_Visa_Approval.constants import MYSQL_ENGINE_URL
+
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+class MySQLConnect:
+    """
+    Class Name :   MySQLConnect
+    Description :   This class establishes a connection to the MySQL database using SQLAlchemy.
+
+    Output      :   Connection to the MySQL database
+    On Failure  :   Raises an exception
+    """
+    engine = None
+
+    def __init__(self) -> None:
+        try:
+            # Get SQLAlchemy engine URL from environment variable
+            sqlalchemy_url = os.getenv(MYSQL_ENGINE_URL)
+            
+            if not sqlalchemy_url:
+                raise Exception("Environment variable 'SQLALCHEMY_ENGINE_URL' is not set.")
+            
+            if MySQLConnect.engine is None:
+                # Initialize the SQLAlchemy engine
+                MySQLConnect.engine = create_engine(sqlalchemy_url)
+            
+            self.engine = MySQLConnect.engine
+            logging.info("MySQL connection successful")
+        
+        except SQLAlchemyError as e:
+            raise USvisaException(f"MySQL connection error: {e}", sys)
+        except Exception as e:
+            raise USvisaException(e, sys)
