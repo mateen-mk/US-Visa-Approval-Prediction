5fa5a6171405ff51bb86ac2835587ea8d333c932
Mateen Khan <mateenmk2428@gmail.com>
2024-12-27 19:44:43 +0530

    src/components/model_pusher.py added

--- src/US_Visa_Approval/components/model_pusher.py
+++ src/US_Visa_Approval/components/model_pusher.py
@@ -1,10 +1,11 @@
 import sys
-import os
-import shutil
+
+from src.US_Visa_Approval.cloud_storage.aws_storage import SimpleStorageService
 from src.US_Visa_Approval.exception import USvisaException
 from src.US_Visa_Approval.logger import logging
 from src.US_Visa_Approval.entity.artifact_entity import ModelPusherArtifact, ModelEvaluationArtifact
 from src.US_Visa_Approval.entity.config_entity import ModelPusherConfig
+from src.US_Visa_Approval.entity.s3_estimator import USvisaEstimator
 
 
 class ModelPusher:
@@ -14,51 +15,35 @@ class ModelPusher:
         :param model_evaluation_artifact: Output reference of data evaluation artifact stage
         :param model_pusher_config: Configuration for model pusher
         """
+        self.s3 = SimpleStorageService()
         self.model_evaluation_artifact = model_evaluation_artifact
         self.model_pusher_config = model_pusher_config
-
-    def save_model_locally(self, source_path: str, destination_path: str) -> None:
-        """
-        Copies the trained model from the source path to the destination path.
-
-        :param source_path: Path of the trained model file
-        :param destination_path: Target path to save the model
-        """
-        try:
-            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-            shutil.copy2(source_path, destination_path)
-            logging.info(f"Model saved locally at: {destination_path}")
-        except Exception as e:
-            raise USvisaException(e, sys) from e
+        self.usvisa_estimator = USvisaEstimator(bucket_name=model_pusher_config.bucket_name,
+                                model_path=model_pusher_config.s3_model_key_path)
 
     def initiate_model_pusher(self) -> ModelPusherArtifact:
         """
-        Method Name :   initiate_model_pusher
-        Description :   This function saves the trained model to a local directory.
-
-        Output      :   Returns a model pusher artifact
+        Method Name :   initiate_model_evaluation
+        Description :   This function is used to initiate all steps of the model pusher
+        
+        Output      :   Returns model evaluation artifact
         On Failure  :   Write an exception log and then raise an exception
         """
-        logging.info("Entered initiate_model_pusher method of ModelPusher class")
+        logging.info("Entered initiate_model_pusher method of ModelTrainer class")
 
         try:
-            logging.info("Saving trained model to local folder")
+            logging.info("Uploading artifacts folder to s3 bucket")
 
-            # Save the model locally
-            self.save_model_locally(
-                source_path=self.model_evaluation_artifact.trained_model_path,
-                destination_path=self.model_pusher_config.model_pusher_path
-            )
+            self.usvisa_estimator.save_model(from_file=self.model_evaluation_artifact.trained_model_path)
 
-            # Create a ModelPusherArtifact
-            model_pusher_artifact = ModelPusherArtifact(
-                model_pusher_path=self.model_pusher_config.model_pusher_path
-            )
 
-            logging.info("Model saved to local folder successfully")
+            model_pusher_artifact = ModelPusherArtifact(bucket_name=self.model_pusher_config.bucket_name,
+                                                        s3_model_path=self.model_pusher_config.s3_model_key_path)
+
+            logging.info("Uploaded artifacts folder to s3 bucket")
             logging.info(f"Model pusher artifact: [{model_pusher_artifact}]")
-            logging.info("Exited initiate_model_pusher method of ModelPusher class")
-
+            logging.info("Exited initiate_model_pusher method of ModelTrainer class")
+            
             return model_pusher_artifact
         except Exception as e:
             raise USvisaException(e, sys) from e
